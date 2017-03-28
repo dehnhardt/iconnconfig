e2f3ba2b12fde227866ebbe46eb36571cb5750f3
Holger Dehnhardt <holger@dehnhardt.org>
2017-03-28 22:27:40 +0200

    retrieve information about device

--- src/sysex/deviceinfo.cpp
+++ src/sysex/deviceinfo.cpp
@@ -1,6 +1,10 @@
 #include "deviceinfo.h"
 
-DeviceInfo::DeviceInfo()
-{
+DeviceInfo::DeviceInfo(Device *device)
+    : SysExMessage(SysExMessage::GET_INFO, SysExMessage::QUERY, device) {}
 
+BYTE_VECTOR *DeviceInfo::getMessageData() {
+  BYTE_VECTOR *messageData = new BYTE_VECTOR();
+  messageData->push_back(this->infoItem);
+  return messageData;
 }
--- src/sysex/deviceinfo.h
+++ src/sysex/deviceinfo.h
@@ -1,11 +1,25 @@
 #ifndef DEVICEINFO_H
 #define DEVICEINFO_H
 
+#include "sysexmessage.h"
 
-class DeviceInfo : public SysExMessage
-{
+class DeviceInfo : public SysExMessage {
 public:
-    DeviceInfo();
+  enum DeviceInfoItem {
+    ACESSORY_NAME = 0x01, /*!< Name of acessory */
+    MANUFACTURER_NAME,    /*!< Name of device manufacturer manufacturer */
+    MODEL_NUMBER,         /*!< Devices modelnumber */
+    SERIAL_NUMBER,        /*!< Devices serial number */
+    FIRMWARE_VERSION,     /*!< Installed firmware version */
+    HARDWARE_VERSION,     /*!< Hardware version of device */
+    DEVICE_NAME = 0x10    /*!< Name of device (writable) */
+  };
+  DeviceInfo(Device *device);
+  void setInfoItem(DeviceInfoItem infoItem) { this->infoItem = infoItem; }
+  BYTE_VECTOR *getMessageData();
+
+private:
+  DeviceInfoItem infoItem = DEVICE_NAME;
 };
 
 #endif // DEVICEINFO_H
