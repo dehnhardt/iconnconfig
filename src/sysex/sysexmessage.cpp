ce0687ba03ed9f990ae763d883a5c8b6cb1edf67
Holger Dehnhardt <holger@dehnhardt.org>
2017-04-03 21:50:31 +0200

    define expected response commands check validity of response commands

--- src/sysex/sysexmessage.cpp
+++ src/sysex/sysexmessage.cpp
@@ -3,6 +3,30 @@
 #include <iostream>
 #include <unistd.h>
 
+/*
+ * Structure and naming od sysex data
+ *
+ * SYSEX_START 1Byte
+ * MANUFACTURER_CODE 3Byte--| Header
+ * MESSAGE_CLASS 1Byte ------
+ *
+ * PRODUCT_ID 2Byte --------| DeviceId -
+ * SERIAL_NUMBER 5 Byte -----          |
+ *                                     |
+ * TRANSACTION_ID 2Byte ----|          | Body (Relevant for Checksum)
+ * COMMAND 2Byte            |          |
+ * DATA_LENGTH 2Byte        |          |
+ * COMMAND_DATA xByte -------          |
+ *                          -----------|
+ * X_SUM 1yte
+ *
+ * SYSEX_END 1Byte
+ *
+ * ST| Man.Id |MC| PID | Serial No.   | TID | CID | LEN  | Data|CS|SE
+ * 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17   ...  .. ..
+ * F0 00 01 73 7E xx xx xx xx xx xx xx xx xx xx xx xx xx  xxxx  xx F7
+ */
+
 SysExMessage::SysExMessage(Command cmd, CommandFlags flags, Device *device)
     : cmd(cmd), cmdflags(flags), device(device) {
   if (this->device != 0)
@@ -12,6 +36,21 @@ SysExMessage::SysExMessage(Command cmd, CommandFlags flags, Device *device)
   command = new BYTE_VECTOR();
   command->push_back(flags);
   command->push_back(cmd);
+  acceptedAnswers = commandAcceptedAnswers[cmd];
+}
+
+SysExMessage::SysExMessage(Command cmd, std::vector<unsigned char> *message,
+                           Device *device)
+    : cmd(cmd), device(device) {
+  if (this->device != 0)
+    this->deviceHeader = this->device->getDeviceHeader();
+  else
+    deviceHeader = new BYTE_VECTOR();
+  cmdflags = (*message)[14];
+  command = new BYTE_VECTOR();
+  command->push_back(cmdflags);
+  command->push_back(cmd);
+  acceptedAnswers = commandAcceptedAnswers[cmd];
 }
 
 SysExMessage::~SysExMessage() {
@@ -23,24 +62,6 @@ BYTE_VECTOR *SysExMessage::getMIDISysExMessage() {
   BYTE_VECTOR *body = new BYTE_VECTOR();
   BYTE_VECTOR *message = new BYTE_VECTOR();
   BYTE_VECTOR *manufacturerHeader = Device::getManufacturerHeader();
-
-  /*
-   * SYSEX_START 1Byte
-   * MANUFACTURER_CODE 3Byte--| Header
-   * MESSAGE_CLASS 1Byte ------
-   *
-   * PRODUCT_ID 2Byte --------| DeviceId -
-   * SERIAL_NUMBER 5 Byte -----          |
-   *                                     |
-   * TRANSACTION_ID 2Byte ----|          | Body (Relevant for Checksum)
-   * COMMAND 2Byte            |          |
-   * DATA_LENGTH 2Byte        |          |
-   * COMMAND_DATA xByte -------          |
-   *                          -----------|
-   * X_SUM 1yte
-   *
-   * SYSEX_END 1Byte
-   */
 
   BYTE_VECTOR *md = getMessageData();
   int mdSize = md->size();
@@ -70,5 +91,27 @@ BYTE_VECTOR *SysExMessage::getMIDISysExMessage() {
 }
 
 void SysExMessage::parseAnswer(BYTE_VECTOR *answer) {
-  std::cout << "Answer: " << answer->size();
+  std::cout << "Answer: " << answer->size() << std::endl;
+  BYTE_VECTOR *commandBytes =
+      new BYTE_VECTOR(answer->begin() + 14, answer->begin() + 16);
+  MIDISysexValue command = MIDISysexValue(commandBytes);
+  if (checkAnswerValid(command.getLongValue())) {
+    std::cout << "Answer (command: " << command.getLongValue() << ") accepted "
+              << std::endl;
+  } else {
+    std::cout << "Answer (command: " << command.getLongValue()
+              << ") not accepted " << std::endl;
+  }
 }
+
+bool SysExMessage::checkAnswerValid(long answerCommandId) {
+  return std::find(acceptedAnswers.begin(), acceptedAnswers.end(),
+                   answerCommandId) != acceptedAnswers.end();
+}
+
+CommandAcceptedAnswers SysExMessage::commandAcceptedAnswers = {
+    {GET_DEVICE, AcceptedAnswers{RET_DEVICE}},
+    {GET_COMMAND_LIST, AcceptedAnswers{RET_COMMAND_LIST}},
+    {GET_INFO_LIST, AcceptedAnswers{RET_INFO_LIST}},
+    {GET_DEVICE_INFO, AcceptedAnswers{RET_SET_DEVICE_INFO}},
+    {RET_SET_DEVICE_INFO, AcceptedAnswers{ACK}}};
--- src/sysex/sysexmessage.h
+++ src/sysex/sysexmessage.h
@@ -4,24 +4,45 @@
 #include "../device.h"
 #include "midi.h"
 
+#include <map>
 #include <vector>
 
+typedef std::vector<int> AcceptedAnswers;
+typedef std::map<int, AcceptedAnswers> CommandAcceptedAnswers;
+
+/**
+ * @brief The SysExMessage class
+ */
 class SysExMessage {
+
+  // enums
 public:
+  /**
+ * @brief The Command enum
+ */
   enum Command {
-    GET_DEVICE = 0x01, /*!< Query for devices  */
-    RET_DEVICE,        /*!< Answer from device to GET_DEVICE query*/
-    GET_COMMAND_LIST,  /*!< Request devices command list. */
-    RET_COMMAND_LIST,  /*!< List of commands supported by device. */
-    GET_INFO_LIST,     /*!< Request list of infos, whch can be retrieved */
-    RET_INFO_LIST,     /*!< List of infos supported by this device */
-    GET_INFO,          /*!< Get a single information from device */
-    RET_SET_INFO,      /*!< Retrun a single information from device see
-                          DeviceInfoItem enum in Info-Class*/
+    GET_DEVICE = 0x01,   /*!< Query for devices  */
+    RET_DEVICE,          /*!< Answer from device to GET_DEVICE query*/
+    GET_COMMAND_LIST,    /*!< Request devices command list. */
+    RET_COMMAND_LIST,    /*!< List of commands supported by device. */
+    GET_INFO_LIST,       /*!< Request list of infos, whch can be retrieved */
+    RET_INFO_LIST,       /*!< List of infos supported by this device */
+    GET_DEVICE_INFO,     /*!< Get a single information from device */
+    RET_SET_DEVICE_INFO, /*!< Retrun a single information from device see
+                     DeviceInfoItem enum in Info-Class*/
     GET_RESET_LIST, /*!< Request list of reset IDs or an ACK if no reset ids are
                        supported */
-    RET_RESET_LIST  /*!< Return list of reset IDs see ResetListItem enum in
-                       Reset class*/
+    RET_RESET_LIST, /*!< Return list of reset IDs see ResetListItem enum in
+                      Reset class*/
+    GET_SAVE_RESTORE_LIST,  /*!< Request the save / restore ids of the device */
+    RET_SAVE_RESTORE_LIST,  /*!< Returns a list of the save / restore ids of the
+                               device */
+    GET_ETHERNET_PORT_INFO, /*!< Requests the information about a specific
+                               ethernet port */
+    RET_SET_ETHERNET_PORT_INFO, /*!< Returns the information about a specifoc
+                                   ethernet port or sets some values for a
+                                   specifoc ethernet port */
+    ACK /*!< Returns a sucess code (0x00) or an error code (0x01 - 0x03) */
 
   };
 
@@ -30,13 +51,20 @@ public:
     QUERY = 0x40   /*!< Query from device */
   };
 
-  SysExMessage(Command command, CommandFlags flags, Device *device);
+  static CommandAcceptedAnswers commandAcceptedAnswers;
+
+  // methods
+public:
+  SysExMessage(Command cmd, CommandFlags flags, Device *device);
+  SysExMessage(Command cmd, BYTE_VECTOR *message, Device *device);
+
   ~SysExMessage();
   virtual BYTE_VECTOR *getMIDISysExMessage();
-  std::string getAnswerDataAsString();
-  long getAnswerDataAsLong();
-  virtual void parseAnswer(BYTE_VECTOR *answer);
+  std::string getDataAsString();
+  long getDataAsLong();
+  void parseAnswer(BYTE_VECTOR *answer);
 
+  // methods
 protected:
   virtual BYTE_VECTOR *getCommand() { return command; }
   virtual BYTE_VECTOR *getMessageData() { return new BYTE_VECTOR(); }
@@ -50,10 +78,13 @@ protected:
     }
     return transactionId;
   }
+  bool checkAnswerValid(long answerCommandId);
 
+  // members
 protected:
   unsigned char cmd;
   unsigned char cmdflags;
+  AcceptedAnswers acceptedAnswers;
   Device *device = 0;
   BYTE_VECTOR *command = 0;
   BYTE_VECTOR *transactionId = 0;
