809a37f25e096f411f60759c3843f09f962d49f7
Holger Dehnhardt <holger@dehnhardt.org>
2017-04-10 22:10:10 +0200

    add translation functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include "miomain.h"
 #include <QApplication>
+#include <QLibraryInfo>
+#include <QTranslator>
 
 int main(int argc, char *argv[]) {
-  QApplication a(argc, argv);
+  QApplication app(argc, argv);
   QCoreApplication::setOrganizationName("punkt-k");
   QCoreApplication::setOrganizationDomain("www.punkt-k.de");
   QCoreApplication::setApplicationName("Mio Config");
+
+  QTranslator qtTranslator;
+  qtTranslator.load("qt_" + QLocale::system().name(),
+                    QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+  app.installTranslator(&qtTranslator);
+
+  QTranslator myappTranslator;
+  myappTranslator.load("myapp_" + QLocale::system().name());
+  app.installTranslator(&myappTranslator);
+
   MioMain w;
   w.show();
-  return a.exec();
+  return app.exec();
 }
