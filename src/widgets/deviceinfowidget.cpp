7487510d70a05b3ff5bd3ce3b7a97a2a6ac40aeb
Holger Dehnhardt <holger@dehnhardt.org>
2017-04-10 22:12:59 +0200

    changed table to simple list disallow moving or floating of dockable windows

--- src/widgets/deviceinfowidget.cpp
+++ src/widgets/deviceinfowidget.cpp
@@ -4,16 +4,8 @@
 DeviceInfoWidget::DeviceInfoWidget(QWidget *parent, Device *device)
     : QDockWidget(parent), ui(new Ui::DeviceInfoWidget), device(device) {
   ui->setupUi(this);
-  QTableWidgetItem *item_0_0 = new QTableWidgetItem("Name");
-  ui->deciceInfoTable->setItem(0, 0, item_0_0);
-  QTableWidgetItem *item_0_1 =
-      new QTableWidgetItem(QString::fromStdString(device->getDeviceName()));
-  ui->deciceInfoTable->setItem(0, 1, item_0_1);
-  QTableWidgetItem *item_1_0 = new QTableWidgetItem("Model");
-  ui->deciceInfoTable->setItem(1, 0, item_1_0);
-  QTableWidgetItem *item_1_1 =
-      new QTableWidgetItem(QString::fromStdString(device->getModelName()));
-  ui->deciceInfoTable->setItem(1, 1, item_1_1);
+  ui->infoList->addItem(tr("Global"));
+  ui->infoList->addItem(tr("Networking"));
 }
 
 DeviceInfoWidget::~DeviceInfoWidget() { delete ui; }
