47c6f6b89a50e92fb5a28e0cead2c56704c296a9
Holger Dehnhardt <holger@dehnhardt.org>
2017-04-11 23:12:56 +0200

    updated some translations

--- src/widgets/deviceinfowidget.cpp
+++ src/widgets/deviceinfowidget.cpp
@@ -4,6 +4,9 @@
 DeviceInfoWidget::DeviceInfoWidget(QWidget *parent, Device *device)
     : QDockWidget(parent), ui(new Ui::DeviceInfoWidget), device(device) {
   ui->setupUi(this);
+  QWidget *lTitleBar = titleBarWidget();
+  setTitleBarWidget(new QWidget());
+  delete lTitleBar;
   ui->infoList->addItem(tr("Global"));
   ui->infoList->addItem(tr("Networking"));
 }
