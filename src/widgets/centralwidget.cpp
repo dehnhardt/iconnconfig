fe714786336f2c2d0d248efecea375951fdce924
Holger Dehnhardt <holger@dehnhardt.org>
2017-04-24 22:35:25 +0200

    added central panel as a container for information widgets

---
+++ src/widgets/centralwidget.cpp
@@ -0,0 +1,6 @@
+#include "centralwidget.h"
+
+centralwidget::centralwidget()
+{
+
+}
---
+++ src/widgets/centralwidget.h
@@ -0,0 +1,12 @@
+#ifndef CENTRALWIDGET_H
+#define CENTRALWIDGET_H
+
+#include <QObject>
+
+class centralwidget : public DockWidget
+{
+public:
+    centralwidget();
+};
+
+#endif // CENTRALWIDGET_H
