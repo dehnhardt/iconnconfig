331f3257f8bbf07265a9f26fc3f37d896d6286cb
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-09 23:12:33 +0200

    make method createWidget abstract

--- src/widgets/multiinfowidget.h
+++ src/widgets/multiinfowidget.h
@@ -44,15 +44,7 @@ public:
 protected:
   QWidget *getWidget(std::string infoName);
   void openLastSelectedSection();
-  virtual QWidget *createWidget(std::string infoName) {
-    QWidget *w = new QWidget(this->parentWidget());
-    QGridLayout *lo = new QGridLayout();
-    w->setLayout(lo);
-    QLabel *l = new QLabel(w);
-    l->setText(QString::fromStdString(infoName));
-    lo->addWidget(l, 0, 0);
-    return w;
-  }
+	virtual QWidget *createWidget(std::string infoName) = 0;
 
 signals:
   void infoChanged(int row);
