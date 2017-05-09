6260327b9c7f65514f5a3cef1bde148154a9cc99
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-09 23:09:56 +0200

    corrected ACESSORY_NAME -> ACCESSORY_NAME

--- src/sysex/sysexmessage.h
+++ src/sysex/sysexmessage.h
@@ -64,13 +64,13 @@ public:
   };
 
   enum DeviceInfoItem {
-    ACESSORY_NAME = 0x01, /*!< Name of acessory */
-    MANUFACTURER_NAME,    /*!< Name of device manufacturer manufacturer */
-    MODEL_NUMBER,         /*!< Devices modelnumber */
-    SERIAL_NUMBER,        /*!< Devices serial number */
-    FIRMWARE_VERSION,     /*!< Installed firmware version */
-    HARDWARE_VERSION,     /*!< Hardware version of device */
-    DEVICE_NAME = 0x10    /*!< Name of device (writable) */
+		ACCESSORY_NAME = 0x01, /*!< Name of acessory */
+		MANUFACTURER_NAME,     /*!< Name of device manufacturer manufacturer */
+		MODEL_NUMBER,          /*!< Devices modelnumber */
+		SERIAL_NUMBER,         /*!< Devices serial number */
+		FIRMWARE_VERSION,      /*!< Installed firmware version */
+		HARDWARE_VERSION,      /*!< Hardware version of device */
+		DEVICE_NAME = 0x10     /*!< Name of device (writable) */
   };
 
   enum CommandFlags {
