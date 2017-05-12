c64be156477b49fe07f828228dead38205749696
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-12 23:52:46 +0200

    ignore length - byte of device name

--- src/sysex/implementedinfos.cpp
+++ src/sysex/implementedinfos.cpp
@@ -1,26 +1,28 @@
 #include "implementedinfos.h"
 
 ImplementedInfos::ImplementedInfos(Device *device)
-		: SysExMessage(SysExMessage::RET_INFO_LIST, SysExMessage::QUERY, device) {}
+    : SysExMessage(SysExMessage::RET_INFO_LIST, SysExMessage::QUERY, device) {}
 
 void ImplementedInfos::parseAnswerData() {
-	implementedInfos = new std::vector<DeviceInfoItem>();
-	unsigned int nInfosSize = data->size();
-	DeviceInfoItem info;
-	for (unsigned int i = 0; i < nInfosSize; i++) {
-		info = (DeviceInfoItem)data->at(i);
-		if (info != 0)
-			implementedInfos->push_back(info);
-	}
+  implementedInfos = new std::vector<DeviceInfoItem>();
+  unsigned int nInfosSize = data->size();
+  DeviceInfoItem info;
+  for (unsigned int i = 0; i < nInfosSize; ++i) {
+    info = (DeviceInfoItem)data->at(i);
+    if (info != 0)
+      implementedInfos->push_back(info);
+    if (info == 16)
+      ++i;
+  }
 }
 
 bool ImplementedInfos::isInfoImplemented(SysExMessage::DeviceInfoItem info) {
-	if (!implementedInfos)
-		return false;
-	std::vector<DeviceInfoItem>::iterator it;
-	for (it = implementedInfos->begin(); it != implementedInfos->end(); it++) {
-		if (*it == info)
-			return true;
-	}
-	return false;
+  if (!implementedInfos)
+    return false;
+  std::vector<DeviceInfoItem>::iterator it;
+  for (it = implementedInfos->begin(); it != implementedInfos->end(); it++) {
+    if (*it == info)
+      return true;
+  }
+  return false;
 }
