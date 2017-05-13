21ccc951e88d57b3fb566e62b09c12971db8eb93
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-14 00:05:37 +0200

    extracted device info table to separate class

--- src/widgets/infotablewidget.cpp
+++ src/widgets/infotablewidget.cpp
@@ -1,6 +1,55 @@
 #include "infotablewidget.h"
 
-InfoTableWidget::InfoTableWidget(QWidget *parent) : QWidget(parent)
-{
+#include <QGridLayout>
+#include <QHeaderView>
 
+InfoTableWidget::InfoTableWidget(QWidget *parent, DeviceInfo *deviceInfo)
+		: QWidget(parent), deviceInfo(deviceInfo) {
+	QGridLayout *lo = new QGridLayout();
+	QPalette qp;
+	setLayout(lo);
+	if (this->deviceInfo) {
+		std::vector<InfoItem> *infoItems = this->deviceInfo->getDeviceInfos();
+		tw = new QTableWidget(infoItems->size(), 2, this);
+		setupTable();
+		for (unsigned int i = 0; i < infoItems->size(); i++) {
+			InfoItem infoItem = infoItems->at(i);
+			QTableWidgetItem *name = new QTableWidgetItem(infoItem.name.c_str());
+			name->setForeground(qp.dark());
+			QTableWidgetItem *value = new QTableWidgetItem(infoItem.value.c_str());
+			name->setFlags(name->flags() & ~Qt::ItemIsEditable);
+			if (!infoItem.editable) {
+				value->setFlags(value->flags() & ~Qt::ItemIsEditable);
+				value->setForeground(qp.dark());
+			}
+			tw->setItem(i, 0, name);
+			tw->setItem(i, 1, value);
+		}
+		lo->addWidget(tw, 0, 0);
+		connect(tw, &QTableWidget::cellChanged, this,
+						&InfoTableWidget::onDeviceInfoChanged);
+		connect(this, &InfoTableWidget::deviceInfoChanged,
+						((DeviceInfoWidget *)parent), &DeviceInfoWidget::deviceInfoChanged);
+	}
+}
+
+void InfoTableWidget::setupTable() {
+	tw->setHorizontalHeaderItem(0, new QTableWidgetItem(tr("Name")));
+	tw->setHorizontalHeaderItem(1, new QTableWidgetItem(tr("Value")));
+	tw->verticalHeader()->hide();
+	tw->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+}
+
+void InfoTableWidget::onDeviceInfoChanged(int row, int column) {
+	std::cout << "Item changed: row " << row << ", column: " << column
+						<< std::endl;
+	if (column == 1) {
+		std::string val;
+		val = tw->item(row, column)->text().toStdString();
+		std::vector<InfoItem> *infoItems = this->deviceInfo->getDeviceInfos();
+		InfoItem item = infoItems->at(row);
+		SysExMessage::DeviceInfoItem i = item.infoItem;
+		std::cout << "Item " << i << " changed to value " << val << std::endl;
+		this->deviceInfoChanged(i, val);
+	}
 }
--- src/widgets/infotablewidget.h
+++ src/widgets/infotablewidget.h
@@ -1,18 +1,28 @@
 #ifndef INFOTABLEWIDGET_H
 #define INFOTABLEWIDGET_H
 
+#include "../sysex/deviceinfo.h"
+#include "deviceinfowidget.h"
+
 #include <QObject>
+#include <QTableWidget>
 #include <QWidget>
 
-class InfoTableWidget : public QWidget
-{
+class InfoTableWidget : public QWidget {
 	Q_OBJECT
 public:
-	explicit InfoTableWidget(QWidget *parent = 0);
+	explicit InfoTableWidget(QWidget *parent, DeviceInfo *deviceInfo);
 
 signals:
+	void deviceInfoChanged(SysExMessage::DeviceInfoItem item, std::string value);
 
 public slots:
+	void onDeviceInfoChanged(int row, int column);
+
+private:
+	DeviceInfo *deviceInfo = 0;
+	QTableWidget *tw = 0;
+	void setupTable();
 };
 
 #endif // INFOTABLEWIDGET_H
