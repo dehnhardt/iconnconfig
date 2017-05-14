e1983d62ecff770e609c7c0ba1bd258565d5a4ad
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-14 23:39:13 +0200

    extract ip addresses

--- src/sysex/retsetethernetportinfo.cpp
+++ src/sysex/retsetethernetportinfo.cpp
@@ -2,3 +2,31 @@
 
 RetSetEthernetPortInfo::RetSetEthernetPortInfo(Device *device)
     : SysExMessage(RET_SET_ETHERNET_PORT_INFO, QUERY, device) {}
+
+void RetSetEthernetPortInfo::parseAnswerData() {
+	std::cout << "parseAnswerData" << std::endl;
+	unsigned char commandVersionNumber = data->at(0);
+	BYTE_VECTOR *portId = new BYTE_VECTOR(data->begin() + 1, data->begin() + 3);
+	unsigned int method = data->at(3);
+
+	extractIp(4);
+	extractIp(9);
+	extractIp(14);
+	extractIp(19);
+	extractIp(24);
+	extractIp(29);
+}
+
+void RetSetEthernetPortInfo::extractIp(int offset) {
+
+	long address = MIDI::byteJoin(data, offset, 5);
+	int a1 = address & 255;
+	address >>= 8;
+	int a2 = address & 255;
+	address >>= 8;
+	int a3 = address & 255;
+	address >>= 8;
+	int a4 = address & 255;
+	std::cout << std::dec << a4 << "." << a3 << "." << a2 << "." << a1
+						<< std::endl;
+}
--- src/sysex/retsetethernetportinfo.h
+++ src/sysex/retsetethernetportinfo.h
@@ -7,7 +7,11 @@ class RetSetEthernetPortInfo : public SysExMessage {
 public:
   RetSetEthernetPortInfo(Device *device);
   RetSetEthernetPortInfo(Command cmd, BYTE_VECTOR *message, Device *device)
-      : SysExMessage(cmd, message, device) {}
+			: SysExMessage(cmd, message, device) {}
+public:
+  virtual void parseAnswerData();
+private:
+  void extractIp(int offset);
 };
 
 #endif // RETSETETHERNETPORTINFO_H
