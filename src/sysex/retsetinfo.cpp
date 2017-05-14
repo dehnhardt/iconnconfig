34a8bdf9c8a0586136815c970b2f2402d79b0407
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-14 16:01:07 +0200

    parse data to get type and value.

--- src/sysex/retsetinfo.cpp
+++ src/sysex/retsetinfo.cpp
@@ -1,6 +1,46 @@
 #include "retsetinfo.h"
 
-RetSetInfo::RetSetInfo()
-{
+RetSetInfo::RetSetInfo(Device *device)
+    : RetSetInfo(SysExMessage::QUERY, device) {}
 
+RetSetInfo::RetSetInfo(CommandFlags flags, Device *device)
+    : SysExMessage(SysExMessage::GET_COMMAND_LIST, flags, device) {}
+
+SysExMessage::DeviceInfoItem RetSetInfo::getInfoItem() { return infoItem; }
+
+std::string RetSetInfo::getValue() { return value; }
+
+void RetSetInfo::parseAnswerData() {
+  this->infoItem = (SysExMessage::DeviceInfoItem)data->at(0);
+  this->value = std::string(data->begin() + 1, data->end());
+}
+
+bool RetSetInfo::isItemEditable() {
+  switch (infoItem) {
+  case DEVICE_NAME:
+    return true;
+  default:
+    return false;
+  }
+}
+
+std::string RetSetInfo::getItemName() {
+  switch (infoItem) {
+  case ACCESSORY_NAME:
+    return tr("Accessory Name").toStdString();
+  case MANUFACTURER_NAME:
+    return tr("Manufacturer Name").toStdString();
+  case MODEL_NUMBER:
+    return tr("Model Number").toStdString();
+  case SERIAL_NUMBER:
+    return tr("Serial Numer").toStdString();
+  case FIRMWARE_VERSION:
+    return tr("Firmware Version").toStdString();
+  case HARDWARE_VERSION:
+    return tr("Hardware Version").toStdString();
+  case DEVICE_NAME:
+    return tr("Name").toStdString();
+  default:
+    return tr("unknown").toStdString();
+  }
 }
--- src/sysex/retsetinfo.h
+++ src/sysex/retsetinfo.h
@@ -1,12 +1,34 @@
 #ifndef RETSETINFO_H
 #define RETSETINFO_H
 
-#include <QObject>
+#include "sysexmessage.h"
 
-class RetSetInfo : public SysExMessage
-{
+#include <QObject>
+#include <QtCore/QCoreApplication>
+
+class RetSetInfo : public SysExMessage {
+  Q_DECLARE_TR_FUNCTIONS(InfoItem)
+
 public:
-    RetSetInfo();
+  // Constructors
+  RetSetInfo(Device *device);
+  RetSetInfo(CommandFlags flags, Device *device);
+  RetSetInfo(Command cmd, BYTE_VECTOR *message, Device *device)
+      : SysExMessage(cmd, message, device) {}
+
+  // getter
+  SysExMessage::DeviceInfoItem getInfoItem();
+  std::string getValue();
+  bool isItemEditable();
+  std::string getItemName();
+
+protected:
+  void parseAnswerData();
+
+private:
+  // variables
+  SysExMessage::DeviceInfoItem infoItem;
+  std::string value;
 };
 
 #endif // RETSETINFO_H
