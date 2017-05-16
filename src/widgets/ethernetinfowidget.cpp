b63a44cff718fce607546d5c22a862a0f5a06742
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-16 22:18:22 +0200

    structured code

--- src/widgets/ethernetinfowidget.cpp
+++ src/widgets/ethernetinfowidget.cpp
@@ -1,110 +1,164 @@
 #include "ethernetinfowidget.h"
 
+#include <QCheckBox>
 #include <QComboBox>
 #include <QGridLayout>
 #include <QGroupBox>
 #include <QLabel>
 #include <QLineEdit>
+#include <QSpacerItem>
 #include <QTableWidget>
 
 EthernetInfoWidget::EthernetInfoWidget(
     QWidget *parent, RetSetEthernetPortInfo *retSetEthernetPortInfo)
     : QWidget(parent), retSetEthernetPortInfo(retSetEthernetPortInfo) {
-  QGridLayout *lo = new QGridLayout();
-  QRegExp rx("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}");
-  QRegExpValidator regValidator(rx, 0);
+
+  lo = new QGridLayout();
 
   if (retSetEthernetPortInfo) {
-    QLabel *tl = new QLabel(tr("Address Type"));
-    lo->addWidget(tl, 0, 0);
-    QComboBox *methodBox = new QComboBox(this);
-    connect(methodBox, SIGNAL(activated(int)), this,
-            SLOT(comboboxSelected(int)));
-    methodBox->addItem(tr("Static"),
-                       QVariant((int)RetSetEthernetPortInfo::STATIC));
-    methodBox->addItem(tr("Dynamic"),
-                       QVariant((int)RetSetEthernetPortInfo::DYNAMIC));
-    lo->addWidget(methodBox, 0, 1);
-    QGroupBox *staticBox = new QGroupBox(tr("Static Address"), this);
-    connect(this, &EthernetInfoWidget::staticBoxDisabled, staticBox,
-            &QGroupBox::setDisabled);
-
-    QGridLayout *staticLayout = new QGridLayout();
-    QLabel *ipl1 = new QLabel(tr("Static IP"), staticBox);
-    QLineEdit *ip1 = new QLineEdit(staticBox);
-    ip1->setValidator(&regValidator);
-    ip1->setText(QString(retSetEthernetPortInfo
-                             ->getAddress(RetSetEthernetPortInfo::STATIC |
-                                          RetSetEthernetPortInfo::ADDRESS)
-                             .c_str()));
-    staticLayout->addWidget(ipl1, 0, 0);
-    staticLayout->addWidget(ip1, 0, 1);
-
-    QLabel *sml1 = new QLabel(tr("Subnet Mask"), staticBox);
-    QLineEdit *sm1 = new QLineEdit(staticBox);
-    sm1->setValidator(&regValidator);
-    sm1->setText(QString(retSetEthernetPortInfo
-                             ->getAddress(RetSetEthernetPortInfo::STATIC |
-                                          RetSetEthernetPortInfo::SUBNET_MASK)
-                             .c_str()));
-    staticLayout->addWidget(sml1, 1, 0);
-    staticLayout->addWidget(sm1, 1, 1);
-
-    QLabel *gwl1 = new QLabel(tr("Gatewey"), staticBox);
-    QLineEdit *gw1 = new QLineEdit(staticBox);
-    gw1->setValidator(&regValidator);
-    gw1->setText(QString(retSetEthernetPortInfo
-                             ->getAddress(RetSetEthernetPortInfo::STATIC |
-                                          RetSetEthernetPortInfo::GATEWAY)
-                             .c_str()));
-    staticLayout->addWidget(gwl1, 2, 0);
-    staticLayout->addWidget(gw1, 2, 1);
-
-    staticBox->setLayout(staticLayout);
-    lo->addWidget(staticBox, 1, 0, 1, 2);
-
-    QGroupBox *dhcpBox = new QGroupBox(tr("DHCP Address"), this);
-    dhcpBox->setEnabled(false);
-    QGridLayout *dhcpLayout = new QGridLayout();
-
-    QLabel *ipl2 = new QLabel(tr("Static IP"), this);
-    QLineEdit *ip2 = new QLineEdit(this);
-    ip2->setValidator(&regValidator);
-    ip2->setText(QString(retSetEthernetPortInfo
-                             ->getAddress(RetSetEthernetPortInfo::DYNAMIC |
-                                          RetSetEthernetPortInfo::ADDRESS)
-                             .c_str()));
-    dhcpLayout->addWidget(ipl2, 0, 0);
-    dhcpLayout->addWidget(ip2, 0, 1);
-
-    QLabel *sml2 = new QLabel(tr("Subnet Mask"), this);
-    QLineEdit *sm2 = new QLineEdit(this);
-    sm2->setValidator(&regValidator);
-    sm2->setText(QString(retSetEthernetPortInfo
-                             ->getAddress(RetSetEthernetPortInfo::DYNAMIC |
-                                          RetSetEthernetPortInfo::SUBNET_MASK)
-                             .c_str()));
-    dhcpLayout->addWidget(sml2, 1, 0);
-    dhcpLayout->addWidget(sm2, 1, 1);
-
-    QLabel *gwl2 = new QLabel(tr("Gatewey"), this);
-    QLineEdit *gw2 = new QLineEdit(this);
-    gw2->setValidator(&regValidator);
-    gw2->setText(QString(retSetEthernetPortInfo
-                             ->getAddress(RetSetEthernetPortInfo::DYNAMIC |
-                                          RetSetEthernetPortInfo::GATEWAY)
-                             .c_str()));
-    dhcpLayout->addWidget(gwl2, 2, 0);
-    dhcpLayout->addWidget(gw2, 2, 1);
-    dhcpBox->setLayout(dhcpLayout);
-    lo->addWidget(dhcpBox, 2, 0, 1, 2);
-    int index = methodBox->findData((int)retSetEthernetPortInfo->getMethod());
-    if (index != -1) { // -1 for not found
-      methodBox->setCurrentIndex(index);
-      comboboxSelected(index);
-    }
+    createWidgets();
+    setupWidgets();
+    setupLayout();
+    setData();
+    createConnections();
   }
   setLayout(lo);
+}
+
+void EthernetInfoWidget::createWidgets() {
+
+  lo = new QGridLayout();
+  staticLayout = new QGridLayout();
+  dhcpLayout = new QGridLayout();
+
+  methodBox = new QComboBox(this);
+
+  staticBox = new QGroupBox(tr("Static Address"), this);
+  dhcpBox = new QGroupBox(tr("DHCP Address"), this);
+
+  tl = new QLabel(tr("Address Type"));
+  ipl1 = new QLabel(tr("Static IP"), staticBox);
+  sml1 = new QLabel(tr("Subnet Mask"), staticBox);
+  gwl1 = new QLabel(tr("Gatewey"), staticBox);
+  ipl2 = new QLabel(tr("Static IP"), this);
+  sml2 = new QLabel(tr("Subnet Mask"), this);
+  gwl2 = new QLabel(tr("Gatewey"), this);
+
+  ip1 = new QLineEdit(staticBox);
+  sm1 = new QLineEdit(staticBox);
+  gw1 = new QLineEdit(staticBox);
+  ip2 = new QLineEdit(dhcpBox);
+  sm2 = new QLineEdit(dhcpBox);
+  gw2 = new QLineEdit(dhcpBox);
+
+  empty = new QWidget();
+}
+
+void EthernetInfoWidget::setupWidgets() {
+
+  QString ipRange = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])";
+  QRegExp ipRegex("^" + ipRange + "\\." + ipRange + "\\." + ipRange + "\\." +
+                  ipRange + "$");
+  QRegExpValidator regValidator(ipRegex, 0);
+
+  ip1->setInputMask("000.000.000.000;0");
+  ip1->setValidator(&regValidator);
+
+  sm1->setInputMask("000.000.000.000;0");
+  sm1->setValidator(&regValidator);
+
+  gw1->setInputMask("000.000.000.000;0");
+  gw1->setValidator(&regValidator);
+
+  ip2->setInputMask("000.000.000.000;0");
+  ip2->setValidator(&regValidator);
+
+  sm2->setInputMask("000.000.000.000;0");
+  sm2->setValidator(&regValidator);
+
+  gw2->setInputMask("000.000.000.000;0");
+  gw2->setValidator(&regValidator);
+
+  empty->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+
+  connect(methodBox, SIGNAL(activated(int)), this, SLOT(comboboxSelected(int)));
+  connect(this, &EthernetInfoWidget::staticBoxDisabled, staticBox,
+          &QGroupBox::setDisabled);
+}
+
+void EthernetInfoWidget::setupLayout() {
+
+  lo->addWidget(tl, 0, 0);
+  lo->addWidget(methodBox, 0, 1);
+
+  staticLayout->addWidget(ipl1, 0, 0);
+  staticLayout->addWidget(ip1, 0, 1);
+
+  staticLayout->addWidget(sml1, 1, 0);
+  staticLayout->addWidget(sm1, 1, 1);
+
+  staticLayout->addWidget(gwl1, 2, 0);
+  staticLayout->addWidget(gw1, 2, 1);
+
+  dhcpLayout->addWidget(ipl2, 0, 0);
+  dhcpLayout->addWidget(ip2, 0, 1);
+
+  dhcpLayout->addWidget(sml2, 1, 0);
+  dhcpLayout->addWidget(sm2, 1, 1);
+
+  dhcpLayout->addWidget(gwl2, 2, 0);
+  dhcpLayout->addWidget(gw2, 2, 1);
+
+  staticBox->setLayout(staticLayout);
+  dhcpBox->setLayout(dhcpLayout);
+
+  lo->addWidget(staticBox, 1, 0, 1, 2);
+  lo->addWidget(dhcpBox, 2, 0, 1, 2);
+  lo->addWidget(empty, 3, 0, 1, 2);
+}
+
+void EthernetInfoWidget::setData() {
+  methodBox->addItem(tr("Static"),
+                     QVariant((int)RetSetEthernetPortInfo::STATIC));
+  methodBox->addItem(tr("Dynamic"),
+                     QVariant((int)RetSetEthernetPortInfo::DYNAMIC));
+
+  ip1->setText(QString(retSetEthernetPortInfo
+                           ->getAddress(RetSetEthernetPortInfo::STATIC |
+                                        RetSetEthernetPortInfo::ADDRESS)
+                           .c_str()));
+  sm1->setText(QString(retSetEthernetPortInfo
+                           ->getAddress(RetSetEthernetPortInfo::STATIC |
+                                        RetSetEthernetPortInfo::SUBNET_MASK)
+                           .c_str()));
+  gw1->setText(QString(retSetEthernetPortInfo
+                           ->getAddress(RetSetEthernetPortInfo::STATIC |
+                                        RetSetEthernetPortInfo::GATEWAY)
+                           .c_str()));
+  ip2->setText(QString(retSetEthernetPortInfo
+                           ->getAddress(RetSetEthernetPortInfo::DYNAMIC |
+                                        RetSetEthernetPortInfo::ADDRESS)
+                           .c_str()));
+  sm2->setText(QString(retSetEthernetPortInfo
+                           ->getAddress(RetSetEthernetPortInfo::DYNAMIC |
+                                        RetSetEthernetPortInfo::SUBNET_MASK)
+                           .c_str()));
+  gw2->setText(QString(retSetEthernetPortInfo
+                           ->getAddress(RetSetEthernetPortInfo::DYNAMIC |
+                                        RetSetEthernetPortInfo::GATEWAY)
+                           .c_str()));
+
+  int index = methodBox->findData((int)retSetEthernetPortInfo->getMethod());
+  if (index != -1) { // -1 for not found
+    methodBox->setCurrentIndex(index);
+    comboboxSelected(index);
+  }
+}
+
+void EthernetInfoWidget::createConnections() {
+  connect(methodBox, SIGNAL(activated(int)), this, SLOT(comboboxSelected(int)));
+  connect(this, &EthernetInfoWidget::staticBoxDisabled, staticBox,
+          &QGroupBox::setDisabled);
 }
 
 void EthernetInfoWidget::comboboxSelected(int selected) {
--- src/widgets/ethernetinfowidget.h
+++ src/widgets/ethernetinfowidget.h
@@ -3,6 +3,11 @@
 
 #include "../sysex/retsetethernetportinfo.h"
 
+#include <QComboBox>
+#include <QGridLayout>
+#include <QGroupBox>
+#include <QLabel>
+#include <QLineEdit>
 #include <QWidget>
 
 class EthernetInfoWidget : public QWidget {
@@ -19,6 +24,41 @@ public slots:
 
 private:
   RetSetEthernetPortInfo *retSetEthernetPortInfo = 0;
+
+  QRegExpValidator *regValidator = 0;
+
+  QGridLayout *lo = 0;
+  QGridLayout *staticLayout = 0;
+  QGridLayout *dhcpLayout = 0;
+
+  QLabel *tl = 0;
+  QLabel *ipl1 = 0;
+  QLabel *sml1 = 0;
+  QLabel *gwl1 = 0;
+  QLabel *ipl2 = 0;
+  QLabel *sml2 = 0;
+  QLabel *gwl2 = 0;
+
+  QComboBox *methodBox = 0;
+
+  QGroupBox *staticBox = 0;
+  QGroupBox *dhcpBox = 0;
+
+  QLineEdit *ip1 = 0;
+  QLineEdit *sm1 = 0;
+  QLineEdit *gw1 = 0;
+  QLineEdit *ip2 = 0;
+  QLineEdit *sm2 = 0;
+  QLineEdit *gw2 = 0;
+
+  QWidget *empty = 0;
+
+private:
+  void createWidgets();
+  void setupWidgets();
+  void setupLayout();
+  void setData();
+  void createConnections();
 };
 
 #endif // ETHERNETINFOWIDGET_H
