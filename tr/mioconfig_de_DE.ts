b3055cddd1a290b709bdfe7f158f20696862e13b
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-21 22:45:48 +0200

    Some translations

--- tr/mioconfig_de_DE.ts
+++ tr/mioconfig_de_DE.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/devicedetection.ui" line="26"/>
         <source>Dialog</source>
-        <translation></translation>
+        <translation>Dialog</translation>
     </message>
 </context>
 <context>
@@ -16,22 +16,23 @@
         <translation type="vanished">Informationen zum Gerät</translation>
     </message>
     <message>
+        <location filename="../src/widgets/deviceinfowidget.cpp" line="23"/>
         <source>Global</source>
-        <translation type="vanished">Allgemein</translation>
+        <translation>Allgemein</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/deviceinfowidget.cpp" line="29"/>
+        <source>Network</source>
+        <translation>Netzwerk</translation>
     </message>
     <message>
         <source>Networking</source>
         <translation type="vanished">Netzwerk</translation>
     </message>
     <message>
-        <location filename="../src/widgets/deviceinfowidget.h" line="11"/>
+        <location filename="../src/widgets/deviceinfowidget.h" line="13"/>
         <source>Device Information</source>
         <translation>Geräteinformationen</translation>
-    </message>
-    <message>
-        <location filename="../src/widgets/deviceinfowidget.cpp" line="27"/>
-        <source>Name</source>
-        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -43,6 +44,112 @@
     <message>
         <source>Hello</source>
         <translation type="vanished">Hallo</translation>
+    </message>
+</context>
+<context>
+    <name>EthernetInfoWidget</name>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="65"/>
+        <source>Static Address</source>
+        <translation>Statische IP</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="66"/>
+        <source>DHCP Address</source>
+        <translation>DHCP Adresse</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="68"/>
+        <source>Address Type</source>
+        <translation>IP-Adressart</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="69"/>
+        <source>Static IP</source>
+        <translation>Statische IP</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="70"/>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="73"/>
+        <source>Subnet Mask</source>
+        <translation>Subnetz Maske</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="71"/>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="74"/>
+        <source>Gateway</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="72"/>
+        <source>DHCP IP</source>
+        <translation>DHCP IP</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="145"/>
+        <source>Static</source>
+        <translation>Statisch</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="147"/>
+        <source>Dynamic</source>
+        <translation>Dynamisch</translation>
+    </message>
+</context>
+<context>
+    <name>InfoItem</name>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="37"/>
+        <source>Accessory Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="39"/>
+        <source>Manufacturer Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="41"/>
+        <source>Model Number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="43"/>
+        <source>Serial Numer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="45"/>
+        <source>Firmware Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="47"/>
+        <source>Hardware Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="49"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="51"/>
+        <source>unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InfoTableWidget</name>
+    <message>
+        <location filename="../src/widgets/infotablewidget.cpp" line="52"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/infotablewidget.cpp" line="53"/>
+        <source>Value</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -100,14 +207,9 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/widgets/multiinfowidget.h" line="25"/>
+        <location filename="../src/widgets/multiinfowidget.h" line="23"/>
         <source>MultiInfoWidget</source>
         <translation>Informationen</translation>
-    </message>
-    <message>
-        <location filename="../src/widgets/multiinfowidget.cpp" line="38"/>
-        <source>%1</source>
-        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
