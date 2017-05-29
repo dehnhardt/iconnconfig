3dc1bcd2b57fef2cec62864c7c1bd7aae700cc16
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-29 18:23:48 +0200

    corrected paint event so that - the text is always written in center of the button, not the vosible area of the button - the text is only paintet, when it is visible

--- src/widgets/controls/portbutton.cpp
+++ src/widgets/controls/portbutton.cpp
@@ -5,44 +5,44 @@
 #include <QStylePainter>
 
 PortButton::PortButton(const long value, const QString &text,
-											 const PortType portType, QWidget *parent)
-		: QToolButton(parent), value(value), portType(portType) {
-	switch (portType) {
-	case MIDI:
-		icon = QIcon(":/pixmaps/midijack");
-		break;
-	default:
-		break;
-	}
-	setIcon(icon);
-	setText(text);
-	setCheckable(true);
-	setMinimumSize(50, 50);
-	setIconSize(QSize(35, 35));
+                       const PortType portType, QWidget *parent)
+    : QToolButton(parent), value(value), portType(portType) {
+  switch (portType) {
+  case MIDI:
+    icon = QIcon(":/pixmaps/midijack");
+    break;
+  default:
+    break;
+  }
+  setIcon(icon);
+  setText(text);
+  setCheckable(true);
+  setMinimumSize(50, 50);
+  setIconSize(QSize(35, 35));
 }
 
 void PortButton::paintEvent(QPaintEvent *event) {
-	QRect rect = event->rect();
-	QStylePainter p(this);
-	QStyleOptionToolButton opt;
-	initStyleOption(&opt);
-	QPainter painter(this);
-	painter.setRenderHints(QPainter::SmoothPixmapTransform |
-												 QPainter::Antialiasing);
-	p.drawComplexControl(QStyle::CC_ToolButton, opt);
-	QFont font = painter.font();
-	font.setBold(true);
-	font.setPointSize(10);
-	painter.setFont(font);
-	if (isChecked()) {
-		QPen penHText(QColor("#66ff66"));
-		painter.setPen(penHText);
-	} else {
-		QPen penHText(QColor("#ffffff"));
-		painter.setPen(penHText);
-	}
-	painter.setRenderHint(QPainter::Antialiasing);
-	painter.drawText(rect, Qt::AlignCenter, opt.text);
+  QToolButton::paintEvent(event);
+
+  QPainter painter(this);
+  QRect rect = event->rect();
+  QRect rect1 = this->rect();
+
+  painter.setRenderHints(QPainter::SmoothPixmapTransform |
+                         QPainter::Antialiasing);
+  QFont font = painter.font();
+  font.setBold(true);
+  font.setPointSize(10);
+  painter.setFont(font);
+  if (isChecked()) {
+    QPen penHText(QColor("#66ff66"));
+    painter.setPen(penHText);
+  } else {
+    QPen penHText(QColor("#ffffff"));
+    painter.setPen(penHText);
+  }
+  if (rect1.intersects(rect))
+    painter.drawText(rect1, Qt::AlignCenter, this->text());
 }
 
 long PortButton::getValue() const { return value; }
