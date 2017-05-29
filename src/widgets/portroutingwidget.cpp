d0f681a8a7d50744bb984fd5af18cbcaefc64a61
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-29 20:32:06 +0200

    added som usb and ethernet ports

--- src/widgets/portroutingwidget.cpp
+++ src/widgets/portroutingwidget.cpp
@@ -3,19 +3,33 @@
 #include <sstream>
 
 PortRoutingWidget::PortRoutingWidget(QWidget *parent, int numDinPorts)
-		: QWidget(parent), numDinPorts(numDinPorts) {
-	createWidgets();
+    : QWidget(parent), numDinPorts(numDinPorts) {
+  createWidgets();
 }
 
 void PortRoutingWidget::createWidgets() {
-	layout = new QGridLayout(this);
-	for (int i = 0; i < numDinPorts; ++i) {
-		std::stringstream s;
-		s << (i + 1);
-		PortButton *b =
-				new PortButton(i, QString(s.str().c_str()), PortButton::MIDI, this);
-		layout->addWidget(b, 0, i);
-	}
+  layout = new QGridLayout(this);
+  for (int i = 0; i < numDinPorts; ++i) {
+    std::stringstream s;
+    s << (i + 1);
+    PortButton *b =
+        new PortButton(i, QString(s.str().c_str()), PortButton::MIDI, this);
+    layout->addWidget(b, 0, i);
+  }
+  for (int i = 0; i < 10; ++i) {
+    std::stringstream s;
+    s << (i + 1);
+    PortButton *b = new PortButton(i, QString(s.str().c_str()),
+                                   PortButton::USB_DEVICE, this);
+    layout->addWidget(b, 1, i);
+  }
+  for (int i = 0; i < 5; ++i) {
+    std::stringstream s;
+    s << (i + 1);
+    PortButton *b =
+        new PortButton(i, QString(s.str().c_str()), PortButton::ETH, this);
+    layout->addWidget(b, 2, i);
+  }
 }
 
 void PortRoutingWidget::setupWidgets() {}
