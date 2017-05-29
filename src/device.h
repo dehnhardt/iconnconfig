87582e8c880ec6ec18354b698ae31e0992e4e81e
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-29 22:56:31 +0200

    formatting...

--- src/device.h
+++ src/device.h
@@ -17,101 +17,101 @@ class GetInfo;
 
 class Device {
 public:
-  Device(int inPortNumber, int outPortNumber, long serialNumber, int productId);
-  ~Device();
+	Device(int inPortNumber, int outPortNumber, long serialNumber, int productId);
+	~Device();
 
 public:
-  static const long MANUFACTURER_USB_ID = 0x2321;
-  static const char MESSAGE_CLASS = 0x7e;
-  static constexpr char MANUFACTURER_SYSEX_ID[3] = {0x00, 0x01, 0x73};
-  static BYTE_VECTOR *manufacturerHeader;
+	static const long MANUFACTURER_USB_ID = 0x2321;
+	static const char MESSAGE_CLASS = 0x7e;
+	static constexpr char MANUFACTURER_SYSEX_ID[3] = {0x00, 0x01, 0x73};
+	static BYTE_VECTOR *manufacturerHeader;
 
-  static const int DATA_LENGTH_OFFSET = 16;
-  static const int DATA_LENGTH_LENGTH = 2;
-  static const int DATA_OFFSET = 18;
+	static const int DATA_LENGTH_OFFSET = 16;
+	static const int DATA_LENGTH_LENGTH = 2;
+	static const int DATA_OFFSET = 18;
 
-  void sentSysex(BYTE_VECTOR *data);
-  BYTE_VECTOR *retrieveSysex();
-  BYTE_VECTOR *nextTransactionId();
+	void sentSysex(BYTE_VECTOR *data);
+	BYTE_VECTOR *retrieveSysex();
+	BYTE_VECTOR *nextTransactionId();
 
-  // getter
+	// getter
 	bool hasMidiSupport();
 
 	static BYTE_VECTOR *getManufacturerHeader();
-  BYTE_VECTOR *getDeviceHeader();
-  BYTE_VECTOR *getFullHeader();
-  bool queryDeviceInfo();
+	BYTE_VECTOR *getDeviceHeader();
+	BYTE_VECTOR *getFullHeader();
+	bool queryDeviceInfo();
 
-  std::string getModelName() { return modelName; }
-  std::string getDeviceName() { return deviceName; }
-  std::string getSerialNumberString() { return serialNumberString; }
-  std::string getManufacturerName() { return manufacturerName; }
-  std::string getFirmwareVersion() { return firmwareVersion; }
-  std::string getHardwareVersion() { return hardwareVersion; }
-  std::string getModelNumber() { return modelNumber; }
-  int getInPortNumer() { return inPortNumber; }
-  int getOutPortNumer() { return outPortNumber; }
+	std::string getModelName() { return modelName; }
+	std::string getDeviceName() { return deviceName; }
+	std::string getSerialNumberString() { return serialNumberString; }
+	std::string getManufacturerName() { return manufacturerName; }
+	std::string getFirmwareVersion() { return firmwareVersion; }
+	std::string getHardwareVersion() { return hardwareVersion; }
+	std::string getModelNumber() { return modelNumber; }
+	int getInPortNumer() { return inPortNumber; }
+	int getOutPortNumer() { return outPortNumber; }
 
-  MIDISysexValue *getSerialNumber() { return serialNumber; }
-  MIDISysexValue *getProductId() { return productId; }
-  bool getDefault() { return isDefault; }
-  RetCommandList *getCommands() { return commands; }
-  GetInfo *getDeviceInfo() { return deviceInfo; }
+	MIDISysexValue *getSerialNumber() { return serialNumber; }
+	MIDISysexValue *getProductId() { return productId; }
+	bool getDefault() { return isDefault; }
+	RetCommandList *getCommands() { return commands; }
+	GetInfo *getDeviceInfo() { return deviceInfo; }
 	RetSetMidiInfo *getMidiInfo() { return midiInfo; }
 
 #ifdef __MIO_SIMULATE__
-  Device(int inPortNumber, int outPortNumber, long serialNumber, int productId,
-         std::string modelName, std::string deviceName);
-  bool getSimulate() { return deviceIsSimulated; }
-  RetCommandList *simulateCommands();
+	Device(int inPortNumber, int outPortNumber, long serialNumber, int productId,
+				 std::string modelName, std::string deviceName);
+	bool getSimulate() { return deviceIsSimulated; }
+	RetCommandList *simulateCommands();
 #endif
 
-  // setter
-  void setDeviceInformation(std::string modelName, std::string deviceName) {
-    this->modelName = modelName;
-    this->deviceName = deviceName;
-  }
-  void setDefault(bool isDefault) { this->isDefault = isDefault; }
+	// setter
+	void setDeviceInformation(std::string modelName, std::string deviceName) {
+		this->modelName = modelName;
+		this->deviceName = deviceName;
+	}
+	void setDefault(bool isDefault) { this->isDefault = isDefault; }
 #ifdef __MIO_SIMULATE__
-  void setSimulate() { deviceIsSimulated = true; }
+	void setSimulate() { deviceIsSimulated = true; }
 #endif
 
 private:
 #ifdef __MIO_SIMULATE__
-  bool deviceIsSimulated = false;
+	bool deviceIsSimulated = false;
 #endif
-  int inPortNumber;
-  int outPortNumber;
+	int inPortNumber;
+	int outPortNumber;
 
-  int transactionId = 0;
+	int transactionId = 0;
 
-  bool isDefault = false;
+	bool isDefault = false;
 
-  RtMidiIn *midiin = 0;
-  RtMidiOut *midiout = 0;
+	RtMidiIn *midiin = 0;
+	RtMidiOut *midiout = 0;
 
-  MIDISysexValue *serialNumber;
-  MIDISysexValue *productId;
+	MIDISysexValue *serialNumber;
+	MIDISysexValue *productId;
 
-  std::string modelName;
-  std::string deviceName;
-  std::string serialNumberString;
-  std::string manufacturerName;
-  std::string firmwareVersion;
-  std::string hardwareVersion;
-  std::string modelNumber;
+	std::string modelName;
+	std::string deviceName;
+	std::string serialNumberString;
+	std::string manufacturerName;
+	std::string firmwareVersion;
+	std::string hardwareVersion;
+	std::string modelNumber;
 
 	RetSetMidiInfo *midiInfo = 0;
-  RetCommandList *commands = 0;
-  RetInfoList *ii = 0;
-  GetInfo *deviceInfo = 0;
+	RetCommandList *commands = 0;
+	RetInfoList *ii = 0;
+	GetInfo *deviceInfo = 0;
 	std::map<int, std::vector<RetSetMidiPortInfo *>> *midiPortInfos = 0;
 
 	BYTE_VECTOR *deviceHeader = 0;
-  BYTE_VECTOR *fullHeader = 0;
+	BYTE_VECTOR *fullHeader = 0;
 
-  void setupMidi();
-  bool checkSysex(BYTE_VECTOR *data);
+	void setupMidi();
+	bool checkSysex(BYTE_VECTOR *data);
 	void requestMidiPortInfos();
 };
 
