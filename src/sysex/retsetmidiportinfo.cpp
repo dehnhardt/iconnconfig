ddb4b6c284c41d7f9baeda3acfbe5821a7f93e47
Holger Dehnhardt <holger@dehnhardt.org>
2017-05-30 23:08:34 +0200

    parse all data out of sysexmessage

--- src/sysex/retsetmidiportinfo.cpp
+++ src/sysex/retsetmidiportinfo.cpp
@@ -3,3 +3,58 @@
 RetSetMidiPortInfo::RetSetMidiPortInfo(Device *device)
 		: SysExMessage(SysExMessage::RET_SET_MIDI_PORT_INFO, SysExMessage::QUERY,
 									 device) {}
+
+void RetSetMidiPortInfo::parseAnswerData() {
+	portId = MIDI::byteJoin(data, 1, 2);
+	portType = (MidiPortType)data->at(3);
+	jackNumber = data->at(4);
+
+	switch (portType) {
+	case NONE:
+		break;
+	case DIN:
+		portTypeName = "DIN-Port";
+		break;
+	case USB_DEVICE:
+		usbDevicePort = data->at(5);
+		portTypeName = "USB-Device-Port";
+		break;
+	case USB_HOST:
+		usbHostPort = data->at(5);
+		portTypeName = "USB-Host-Port";
+		break;
+	case ETHERNET:
+		ethernetSession = data->at(5);
+		portTypeName = "Ethernet-Port";
+		break;
+	}
+	portNameLength = data->at(8);
+	int portFlags = data->at(9);
+	inputEnabled = ((portFlags & 0x01) == 0x01);
+	outputEnabled = ((portFlags & 0x02) == 0x02);
+	portName = std::string(data->begin() + 10, data->end());
+}
+
+long RetSetMidiPortInfo::getPortId() const { return portId; }
+
+MidiPortType RetSetMidiPortInfo::getPortType() const { return portType; }
+
+bool RetSetMidiPortInfo::getInputEnabled() const { return inputEnabled; }
+
+bool RetSetMidiPortInfo::getOutputEnabled() const { return outputEnabled; }
+
+int RetSetMidiPortInfo::getJackNumber() const { return jackNumber; }
+
+int RetSetMidiPortInfo::getUsbDevicePort() const { return usbDevicePort; }
+
+int RetSetMidiPortInfo::getUsbHostPort() const { return usbHostPort; }
+
+void RetSetMidiPortInfo::setUsbHostPort(int value) { usbHostPort = value; }
+
+int RetSetMidiPortInfo::getEthernetSession() const { return ethernetSession; }
+
+std::string RetSetMidiPortInfo::getPortName() const { return portName; }
+
+void RetSetMidiPortInfo::setPortName(const std::string &value) {
+	portName = value;
+}
--- src/sysex/retsetmidiportinfo.h
+++ src/sysex/retsetmidiportinfo.h
@@ -3,6 +3,8 @@
 
 #include "sysexmessage.h"
 
+enum MidiPortType { NONE, DIN, USB_DEVICE, USB_HOST, ETHERNET };
+
 class RetSetMidiPortInfo : public SysExMessage {
 public:
 	RetSetMidiPortInfo(Device *device);
@@ -10,7 +12,41 @@ public:
 	RetSetMidiPortInfo(Command cmd, BYTE_VECTOR *message, Device *device)
 			: SysExMessage(cmd, message, device) {}
 
-	enum MidiPortType { DIN, USB_DEVICE, USB_HOST, ETHERNET };
+	// enum MidiPortType { NONE, DIN, USB_DEVICE, USB_HOST, ETHERNET };
+
+	void parseAnswerData();
+
+public:
+	// getter
+	long getPortId() const;
+	MidiPortType getPortType() const;
+	bool getInputEnabled() const;
+	bool getOutputEnabled() const;
+	int getJackNumber() const;
+	int getUsbDevicePort() const;
+	int getUsbHostPort() const;
+	void setUsbHostPort(int value);
+	int getEthernetSession() const;
+	std::string getPortName() const;
+
+public:
+	// setter
+	void setPortName(const std::string &value);
+
+private:
+	long portId;
+	MidiPortType portType;
+	bool inputEnabled;
+	bool outputEnabled;
+
+	int jackNumber = 0;
+	int usbDevicePort = 0;
+	int usbHostPort = 0;
+	int ethernetSession = 0;
+
+	int portNameLength = 0;
+	std::string portName;
+	std::string portTypeName;
 };
 
 #endif // RETSETMIDIPORTINFO_H
