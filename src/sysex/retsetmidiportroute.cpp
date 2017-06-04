4407ec56ac036a1759469a9b0f08528e22cbf759
Holger Dehnhardt <holger@dehnhardt.org>
2017-06-04 23:09:29 +0200

    calculation of byte and bit outsourced setting of routing added

--- src/sysex/retsetmidiportroute.cpp
+++ src/sysex/retsetmidiportroute.cpp
@@ -12,13 +12,55 @@ void RetSetMidiPortRoute::parseAnswerData() {
 }
 
 bool RetSetMidiPortRoute::isPortRouted(int portNumber) {
-	--portNumber;
-	unsigned int byte = portNumber / 4;
-	unsigned int bit = portNumber - (byte * 4);
-	if( byte < portRoutings->size() ){
+	int byte = -1;
+	int bit = -1;
+	getPortByteAndBit(portNumber, byte, bit);
+	if (byte <= numerOfExpectedBytes) {
 		unsigned int byteVal = portRoutings->at(byte);
 		int mask = pow(2, bit);
 		return byteVal & mask;
 	}
 	return false;
+}
+
+void RetSetMidiPortRoute::setPortRouted(int portNumber, bool routed) {
+	int byte = -1;
+	int bit = -1;
+	int val = 0;
+	getPortByteAndBit(portNumber, byte, bit);
+	if (byte != -1 && bit != -1)
+		val = portRoutings->at(byte);
+	int mask = pow(2, bit);
+	if (routed) {
+		val |= mask;
+	} else {
+		val &= !mask;
+	}
+}
+
+int RetSetMidiPortRoute::getTotalNumberOfPorts() const {
+	return totalNumberOfPorts;
+}
+
+void RetSetMidiPortRoute::setTotalNumberOfPorts(int value) {
+	totalNumberOfPorts = value;
+	numerOfExpectedBytes = getNumberOfExpectedBytes(totalNumberOfPorts);
+}
+
+std::vector<unsigned char> *RetSetMidiPortRoute::getMessageData()
+{
+	return portRoutings;
+}
+
+void RetSetMidiPortRoute::getPortByteAndBit(int portNumber, int &byte,
+																						int &bit) {
+	--portNumber;
+	if (numerOfExpectedBytes != -1 &&
+			(unsigned int)numerOfExpectedBytes >= portRoutings->size())
+		byte = portNumber / 4;
+	bit = portNumber - (byte * 4);
+}
+
+int RetSetMidiPortRoute::getNumberOfExpectedBytes(int totalPortNumber) {
+	return (((totalPortNumber - 1) / 8) + 1) * 2;
 }
--- src/sysex/retsetmidiportroute.h
+++ src/sysex/retsetmidiportroute.h
@@ -12,10 +12,19 @@ public:
 
 	void parseAnswerData();
 	bool isPortRouted(int portNumber);
+	void setPortRouted(int portNumber, bool routed);
+
+	int getTotalNumberOfPorts() const;
+	void setTotalNumberOfPorts(int value);
+	BYTE_VECTOR *getMessageData();
 
 private:
 	long portId;
+	int totalNumberOfPorts = -1;
+	int numerOfExpectedBytes = -1;
 	BYTE_VECTOR *portRoutings = 0;
+	int getNumberOfExpectedBytes(int totalPortNumber);
+	void getPortByteAndBit(int portNumber, int &byte, int &bit);
 };
 
 #endif // RETSETMIDIPORTROUTE_H
