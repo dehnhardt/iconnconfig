e087c6498c5fd02d75f006b28d0692177a20e967
Holger Dehnhardt <holger@dehnhardt.org>
2017-06-13 19:43:25 +0200

    cleanup code

--- src/widgets/controls/ipaddressinput.cpp
+++ src/widgets/controls/ipaddressinput.cpp
@@ -4,8 +4,6 @@ IPAddressInput::IPAddressInput(QWidget *parent) : QLineEdit(parent) {
 	ipRange = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])";
 	ipRegex = QRegExp("^" + ipRange + "\\." + ipRange + "\\." + ipRange + "\\." +
 										ipRange + "$");
-	//*regValidator = new QRegExpValidator(ipRegex, 0);
-	// setValidator(regValidator);
 	connect(this, SIGNAL(textChanged(QString)), this, SLOT(validate(QString)));
 }
 
