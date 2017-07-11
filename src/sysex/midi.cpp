183177f84aed39b6e32b7c4ddf0c8d30deeb457c
Holger Dehnhardt <holger@dehnhardt.org>
2017-07-11 17:04:31 +0200

    eliminate all warnings

--- src/sysex/midi.cpp
+++ src/sysex/midi.cpp
@@ -7,122 +7,124 @@
 MIDI::MIDI() {}
 
 RtMidiIn *MIDI::createMidiIn(const std::string clientName) {
-  // RtMidiIn constructor
-  RtMidiIn *midiin = 0;
-  try {
-    midiin = new RtMidiIn(RtMidi::LINUX_ALSA, clientName);
-  } catch (RtMidiError &error) {
-    // TODO Handle the exception here
-    error.printMessage();
-  }
-  midiin->ignoreTypes(false, true, true);
-  return midiin;
+	// RtMidiIn constructor
+	RtMidiIn *midiin = 0;
+	try {
+		midiin = new RtMidiIn(RtMidi::LINUX_ALSA, clientName);
+	} catch (RtMidiError &error) {
+		// TODO Handle the exception here
+		error.printMessage();
+	}
+	midiin->ignoreTypes(false, true, true);
+	return midiin;
 }
 
 RtMidiOut *
-MIDI::createMidiOut(const std::string clientName) { // RtMidiOut constructor
-  RtMidiOut *midiout = 0;
-  try {
-    midiout = new RtMidiOut(RtMidi::LINUX_ALSA, clientName);
-  } catch (RtMidiError &error) {
-    // TODO Handle the exception here
-    error.printMessage();
-  }
-  return midiout;
+MIDI::createMidiOut(const std::string clientName) {// RtMidiOut constructor
+	RtMidiOut *midiout = 0;
+	try {
+		midiout = new RtMidiOut(RtMidi::LINUX_ALSA, clientName);
+	} catch (RtMidiError &error) {
+		// TODO Handle the exception here
+		error.printMessage();
+	}
+	return midiout;
 }
 
 unsigned char MIDI::RolandChecksum(BYTE_VECTOR *message) {
-  unsigned int nBytes = message->size();
-  unsigned int sum = 0;
-  for (unsigned int i = 0; i < nBytes; i++) {
-    sum += (int)message->at(i);
-    if (sum > 127)
-      sum -= 128;
-  }
-  return 128 - sum;
+	unsigned long nBytes = message->size();
+	unsigned long sum = 0;
+	for (unsigned int i = 0; i < nBytes; i++) {
+		sum += static_cast<unsigned long>(message->at(i));
+		if (sum > 127)
+			sum -= 128;
+	}
+	return static_cast<unsigned char>(128 - sum);
 }
 
-BYTE_VECTOR *MIDI::byteSplit(long val) { return MIDI::byteSplit(val, 0); }
+BYTE_VECTOR *MIDI::byteSplit(unsigned long val) {
+	return MIDI::byteSplit(val, 0);
+}
 
-BYTE_VECTOR *MIDI::byteSplit(long val, int size) {
-  BYTE_VECTOR *bytes = new BYTE_VECTOR();
-  bytes->reserve(size);
-  while (val > 0) {
-    unsigned char c = val & 0x7f;
-    val >>= 7;
-    bytes->push_back(c);
-  }
-  if (size > 0) {
-    int nLength = bytes->size();
-    if (nLength < size) {
-      for (int i = nLength; i < size; i++)
-        bytes->push_back(0x00);
-    }
-  }
-  std::reverse(bytes->begin(), bytes->end());
-  return bytes;
+BYTE_VECTOR *MIDI::byteSplit(unsigned long val, unsigned long size) {
+	BYTE_VECTOR *bytes = new BYTE_VECTOR();
+	bytes->reserve(size);
+	while (val > 0) {
+		unsigned char c = val & 0x7f;
+		val >>= 7;
+		bytes->push_back(c);
+	}
+	if (size > 0) {
+		unsigned long nLength = bytes->size();
+		if (nLength < size) {
+			for (unsigned long i = nLength; i < size; i++)
+				bytes->push_back(0x00);
+		}
+	}
+	std::reverse(bytes->begin(), bytes->end());
+	return bytes;
 }
 
 long MIDI::byteJoin(BYTE_VECTOR *message) {
-  return byteJoin(message, 0, message->size());
+	return byteJoin(message, 0, message->size());
 }
 
-long MIDI::byteJoin(BYTE_VECTOR *message, unsigned int start,
-                    unsigned int length) {
-  unsigned int cnt;
-  int current = 0;
+long MIDI::byteJoin(BYTE_VECTOR *message, unsigned long start,
+					unsigned long length) {
+	unsigned long cnt;
+	long current = 0;
 
-  if (start + length > message->size())
-    return -1;
+	if (start + length > message->size())
+		return -1;
 
-  for (cnt = start; cnt < start + length; cnt++) {
-    current <<= 7;
-    current += message->at(cnt);
-  }
-  return current;
+	for (cnt = start; cnt < start + length; cnt++) {
+		current <<= 7;
+		current += message->at(cnt);
+	}
+	return current;
 }
 
-std::string MIDI::decodeIp(BYTE_VECTOR *data, int offset) {
+std::string MIDI::decodeIp(BYTE_VECTOR *data, unsigned long offset) {
 
-  long address = MIDI::byteJoin(data, offset, 5);
-  int a1 = address & 255;
-  address >>= 8;
-  int a2 = address & 255;
-  address >>= 8;
-  int a3 = address & 255;
-  address >>= 8;
-  int a4 = address & 255;
-  std::stringstream result;
-  result << std::dec << a4 << "." << a3 << "." << a2 << "." << a1;
-  std::string ad = result.str();
-  return ad;
+	long address = MIDI::byteJoin(data, offset, 5);
+	int a1 = address & 255;
+	address >>= 8;
+	int a2 = address & 255;
+	address >>= 8;
+	int a3 = address & 255;
+	address >>= 8;
+	int a4 = address & 255;
+	std::stringstream result;
+	result << std::dec << a4 << "." << a3 << "." << a2 << "." << a1;
+	std::string ad = result.str();
+	return ad;
 }
 
 BYTE_VECTOR *MIDI::encodeIpAddress(std::string ipAddress) {
-  BYTE_VECTOR *result = 0;
+	BYTE_VECTOR *result = 0;
 
-  long lAddress = 0L;
-  std::istringstream ss(ipAddress);
-  std::string token;
-  std::vector<std::string> ipParts;
+	unsigned long lAddress = 0L;
+	std::istringstream ss(ipAddress);
+	std::string token;
+	std::vector<std::string> ipParts;
 
-  while (std::getline(ss, token, '.')) {
-    ipParts.push_back(std::string(token));
-  }
-  for (std::vector<std::string>::iterator it = ipParts.begin();
-       it != ipParts.end(); ++it) {
-    if (it != ipParts.begin())
-      lAddress <<= 8;
-    token = (*it);
-    lAddress += std::stol(token);
-  }
-  result = MIDI::byteSplit(lAddress, 5);
-  return result;
+	while (std::getline(ss, token, '.')) {
+		ipParts.push_back(std::string(token));
+	}
+	for (std::vector<std::string>::iterator it = ipParts.begin();
+		 it != ipParts.end(); ++it) {
+		if (it != ipParts.begin())
+			lAddress <<= 8;
+		token = (*it);
+		lAddress += static_cast<unsigned long>(std::stol(token));
+	}
+	result = MIDI::byteSplit(lAddress, 5);
+	return result;
 }
 
 void MIDI::printMessage(BYTE_VECTOR *message) {
-  unsigned int nMessageSize = message->size();
-  for (unsigned int i = 0; i < nMessageSize; i++)
-    std::cout << std::hex << (int)message->at(i) << " ";
-  std::cout << "\n" << std::flush;
+	unsigned long nMessageSize = message->size();
+	for (unsigned int i = 0; i < nMessageSize; i++)
+		std::cout << std::hex << static_cast<int>(message->at(i)) << " ";
+	std::cout << "\n" << std::flush;
 }
--- src/sysex/midi.h
+++ src/sysex/midi.h
@@ -10,14 +10,14 @@
 #if defined(__WINDOWS_MM__)
 #include <windows.h>
 #define SLEEP(milliseconds) Sleep((DWORD)milliseconds)
-#else // Unix variants
+#else// Unix variants
 #include <unistd.h>
 #define SLEEP(milliseconds) usleep((unsigned long)(milliseconds * 1000.0))
 #endif
 
 #ifdef __LINUX_ALSA__
 #include <alsa/seq_event.h>
-#endif //__LINUX_ALSA__
+#endif//__LINUX_ALSA__
 
 #define SYSEX_START 0xf0
 #define SYSEX_END 0xf7
@@ -27,61 +27,63 @@
 #define BYTE_VECTOR std::vector<unsigned char>
 #ifdef __LINUX_ALSA__
 enum midiEventType { MIDI_EVENT_SYSEX = SND_SEQ_EVENT_SYSEX };
-#endif // __LINUX_ALSA__
+#endif// __LINUX_ALSA__
 
 class MIDI {
 public:
-  MIDI();
+	MIDI();
 
 public:
-  // Helper Methods
-  static unsigned char RolandChecksum(std::vector<unsigned char> *message);
-  static BYTE_VECTOR *byteSplit(long val, int length);
-  static BYTE_VECTOR *byteSplit(long val);
-  static long byteJoin(BYTE_VECTOR *message);
-  static long byteJoin(BYTE_VECTOR *message, unsigned int start,
-                       unsigned int end);
-  static std::string decodeIp(BYTE_VECTOR *data, int offset);
-  static BYTE_VECTOR *encodeIpAddress(std::string ipAddress);
-  static void printMessage(BYTE_VECTOR *v);
-  static bool compareByteVector(BYTE_VECTOR *v1, BYTE_VECTOR *v2) {
-    if (v1->size() != v2->size())
-      return false;
-    int size = v1->size();
-    for (int i = 0; i < size; i++) {
-      if ((*v1)[i] != (*v2)[i])
-        return false;
-    }
-    return true;
-  }
+	// Helper Methods
+	static unsigned char RolandChecksum(std::vector<unsigned char> *message);
+	static BYTE_VECTOR *byteSplit(unsigned long val, unsigned long length);
+	static BYTE_VECTOR *byteSplit(unsigned long val);
+	static long byteJoin(BYTE_VECTOR *message);
+	static long byteJoin(BYTE_VECTOR *message, unsigned long start,
+						 unsigned long end);
+	static std::string decodeIp(BYTE_VECTOR *data, unsigned long offset);
+	static BYTE_VECTOR *encodeIpAddress(std::string ipAddress);
+	static void printMessage(BYTE_VECTOR *v);
+	static bool compareByteVector(BYTE_VECTOR *v1, BYTE_VECTOR *v2) {
+		if (v1->size() != v2->size())
+			return false;
+		unsigned long size = v1->size();
+		for (unsigned long i = 0; i < size; i++) {
+			if ((*v1)[i] != (*v2)[i])
+				return false;
+		}
+		return true;
+	}
 
-  static RtMidiIn *createMidiIn(
-      const std::string clientName = std::string("MioConfig Input Client"));
-  static RtMidiOut *createMidiOut(
-      const std::string clientName = std::string("MioConfig Output Client"));
+	static RtMidiIn *createMidiIn(
+		const std::string clientName = std::string("MioConfig Input Client"));
+	static RtMidiOut *createMidiOut(
+		const std::string clientName = std::string("MioConfig Output Client"));
 };
 
 class MIDISysexValue {
 public:
-  MIDISysexValue(long val, int length) : longValue(val), byteLength(length) {}
-  MIDISysexValue(long val) : longValue(val) {}
-  // TODO setting the byte value does not set the correct long value
-  MIDISysexValue(BYTE_VECTOR *val) {
-    byteValue = val;
-    longValue = MIDI::byteJoin(byteValue);
-  }
+	MIDISysexValue(long val, unsigned long length)
+		: longValue(val), byteLength(length) {}
+	MIDISysexValue(long val) : longValue(val) {}
+	// TODO setting the byte value does not set the correct long value
+	MIDISysexValue(BYTE_VECTOR *val) {
+		byteValue = val;
+		longValue = MIDI::byteJoin(byteValue);
+	}
 
-  long getLongValue() { return longValue; }
-  BYTE_VECTOR *getByteValue() {
-    if (byteValue == 0)
-      byteValue = MIDI::byteSplit(longValue, byteLength);
-    return byteValue;
-  }
+	long getLongValue() { return longValue; }
+	BYTE_VECTOR *getByteValue() {
+		if (byteValue == 0)
+			byteValue = MIDI::byteSplit(static_cast<unsigned long>(longValue),
+										byteLength);
+		return byteValue;
+	}
 
 private:
-  long longValue;
-  BYTE_VECTOR *byteValue = 0;
-  int byteLength;
+	long longValue;
+	BYTE_VECTOR *byteValue = 0;
+	unsigned long byteLength;
 };
 
-#endif // MIDI_H
+#endif// MIDI_H
