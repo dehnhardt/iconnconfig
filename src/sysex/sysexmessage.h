86ba5ed19d8a511e40e245637c98119c984ce08f
Holger Dehnhardt <holger@dehnhardt.org>
2017-07-11 22:36:35 +0200

    added all midi commands

--- src/sysex/sysexmessage.h
+++ src/sysex/sysexmessage.h
@@ -58,7 +58,7 @@ public:
 		GET_MIDI_INFO =
 			0x20, /*!< Queueries information about the midi parameters */
 		RET_SET_MIDI_INFO, /*!< Returns information about the midi parameters */
-		GET_MIDI_PORT_INFO, /*!< Queuery the information about a specific MID
+		GET_MIDI_PORT_INFO, /*!< Queuery the information about a specific MIDI
 							   Port */
 		RET_SET_MIDI_PORT_INFO, /*!< Returns or set the MIDI-Information
 									 for a specific port */
@@ -67,10 +67,18 @@ public:
 		RET_SET_MIDI_PORT_FILTER, /*!< Returns or writes infomation about
 									 current midi port filters for a specific
 									 port*/
-		GET_MIDI_PORT_REMAP,
-		RET_SET_MIDI_PORT_REMAP,
-		GET_MIDI_PORT_ROUTE,
-		RET_SET_MIDI_PORT_ROUTE
+		GET_MIDI_PORT_REMAP,	  /*!< Queries remaps of MIDI ports */
+		RET_SET_MIDI_PORT_REMAP,  /*!< Set the remapping of midi ports */
+		GET_MIDI_PORT_ROUTE,	  /*!< Queries the routing of a specific port */
+		RET_SET_MIDI_PORT_ROUTE,  /*!< sets the routing destinations from a
+									speific port */
+		GET_MIDI_PORT_DETAIL, /*!< get the details of a specific MIDI port */
+		RET_SET_MIDI_PORT_DETAIL, /*!< set details of a specific MIDI port */
+		GET_RTP_MIDI_CONNECTION_DETAIL, /*!< query details of a RTP MIDI port */
+		RET_RTP_MIDI_CONNECTION_DETAIL, /*!< retrieve the details of an RTP MIDI
+										   port */
+		GET_USB_HOST_MIDI_DETAIL,		/*!< query details of an USB host */
+		RET_USB_HOST_MIDI_DETAIL /*!< retrieve the details of an USB host */
 	};
 
 	enum DeviceInfoItem {
