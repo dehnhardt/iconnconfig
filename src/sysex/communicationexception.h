9044e0fe2e228af0c431e49d80e8385c38503293
Holger Dehnhardt <holger@dehnhardt.org>
2017-08-05 18:52:34 +0200

    build communication exception from RtMidiError

--- src/sysex/communicationexception.h
+++ src/sysex/communicationexception.h
@@ -1,20 +1,52 @@
 #ifndef COMMUNICATIONEXCEPTION_H
 #define COMMUNICATIONEXCEPTION_H
 
+#include "../RtMidi.h"
+
 #include <sstream>
 #include <stdexcept>
 #include <string>
 
 class CommunicationException : public std::runtime_error {
 public:
-	enum CommunicationErrorCode { UNKNOWN = 0, ANSWER_TIMEOOUT = 1 };
+	enum CommunicationErrorCode {
+		UNKNOWN = 0,
+		ANSWER_TIMEOOUT = 1,
+		NO_DEVICES_FOUND,
+		INVALID_DEVICE,
+		INVALID_PARAMETER,
+		INVALID_USE,
+	};
 
 	CommunicationException(CommunicationErrorCode code = UNKNOWN)
-		: std::runtime_error("CommunicationException"), code(code) {}
+			: std::runtime_error("CommunicationException"), code(code) {}
+	CommunicationException(RtMidiError error)
+			: std::runtime_error("CommunicationException") {
+		switch (error.getType()) {
+		case RtMidiError::INVALID_PARAMETER:
+			code = INVALID_PARAMETER;
+			break;
+		case RtMidiError::NO_DEVICES_FOUND:
+			code = NO_DEVICES_FOUND;
+			break;
+		case RtMidiError::INVALID_DEVICE:
+			code = INVALID_DEVICE;
+			break;
+		case RtMidiError::INVALID_USE:
+			code = INVALID_USE;
+			break;
+		default:
+			break;
+		}
+		errorString = error.getMessage();
+	}
+
 	virtual ~CommunicationException();
 	// getter
 	CommunicationErrorCode getErrorCode() { return code; }
 	std::string getErrorMessage() {
+		if (errorString.size() > 0)
+			return errorString;
 		std::stringstream e;
 		e << "Error when communicationg with device. Reason: ";
 		switch (code) {
@@ -24,7 +56,17 @@ public:
 		case ANSWER_TIMEOOUT:
 			e << " timeout whil waiting for the answer. ";
 			break;
+		case INVALID_PARAMETER:
+			e << " the port number given is invalid. ";
+			break;
+		case NO_DEVICES_FOUND:
+			e << " no devices are found. ";
+			break;
+		case INVALID_USE:
+			e << " invalid use of the RtMidiApi. ";
+			break;
 		default:
+			e << " unspecified error";
 			break;
 		}
 		e << std::endl;
@@ -33,6 +75,7 @@ public:
 
 private:
 	CommunicationErrorCode code = UNKNOWN;
+	std::string errorString;
 };
 
-#endif// COMMUNICATIONEXCEPTION_H
+#endif // COMMUNICATIONEXCEPTION_H
