9091fed50258b898507bad5b3968ed298024f7ed
Olivier Humbert <trebmuh@users.noreply.github.com>
2019-02-13 00:06:45 +0100

    Adds French translation

---
+++ tr/iconnconfig_fr_FR.ts
@@ -0,0 +1,424 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="26"/>
+        <source>About...</source>
+        <translation>À propos...</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="43"/>
+        <source>Application Name</source>
+        <translation>Nom de l'application</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="45"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="47"/>
+        <source>Author</source>
+        <translation>Auteur</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="49"/>
+        <source>Organization</source>
+        <translation>Organisation</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="51"/>
+        <source>License</source>
+        <translation>Licence</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="53"/>
+        <source>Used Software</source>
+        <translation>Logiciels utilisés</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="55"/>
+        <source>RtMIDI</source>
+        <translation>RtMidi</translation>
+    </message>
+    <message>
+        <source>All midi input and output is realized with the RtMidi Library. Created at &lt;a href=&apos;https://ccrma.stanford.edu/software/stk/classRtMidi.html&apos;&gt;Stanford University&lt;/a&gt; &lt;br/&gt;Copyright ©1995--2016 Perry R. Cook and Gary P. Scavone. All Rights Reserved.</source>
+        <translation type="vanished">Toutes les entrées et sorties MIDI sont réalisées avec la bibliothèque RtMidi. Créée à l&apos;&lt;a href=&apos;https://ccrma.stanford.edu/software/stk/classRtMidi.html&apos;&gt;université Stanford&lt;/a&gt; &lt;br/&gt;Copyright ©1995--2016 Perry R. Cook et Gary P. Scavone. Tout droit réservé.</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="56"/>
+        <source>All midi in- and output is realized with the RtMidi Library. Created at &lt;a href=&apos;https://ccrma.stanford.edu/software/stk/classRtMidi.html&apos;&gt;Stanford University&lt;/a&gt; &lt;br/&gt;Copyright ©1995--2016 Perry R. Cook and Gary P. Scavone. All Rights Reserved.</source>
+        <translation>Toutes les entrées et sorties MIDI sont réalisées avec la bibliothèque RtMidi. Créée à l&apos;&lt;a href=&apos;https://ccrma.stanford.edu/software/stk/classRtMidi.html&apos;&gt;université Stanford&lt;/a&gt; &lt;br/&gt;Copyright ©1995--2016 Perry R. Cook et Gary P. Scavone. Tout droit réservé.</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="63"/>
+        <source>Used Icons</source>
+        <translation>Icônes utilisés</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="65"/>
+        <source>USB Device Jack Icon</source>
+        <translation>Icône Jack périphérique USB</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="65"/>
+        <source>Downloaded from the &lt;a href=&apos;https://thenounproject.com/&apos;&gt;NOUN Project&lt;/a&gt;&lt;br/&gt;
+Created by Okan Benn</source>
+        <translation>Téléchargé depuis le &lt;a href=&apos;https://thenounproject.com/&apos;&gt;Projet NOUN&lt;/a&gt;&lt;br/&gt;
+Erstellt von Okan Benn</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="70"/>
+        <source>Ethernet Jack Icon</source>
+        <translation>Icône Jack Ethernet</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="70"/>
+        <source>Downloaded from the &lt;a href=&apos;https://thenounproject.com/&apos;&gt;NOUN Project&lt;/a&gt;&lt;br/&gt;
+Created by Michael Wohlwend</source>
+        <translation>Téléchargé depuis le &lt;a href=&apos;https://thenounproject.com/&apos;&gt;Projet NOUN&lt;/a&gt;&lt;br/&gt;
+Erstelt von Michael Wohlwend</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="74"/>
+        <source>Credits</source>
+        <translation>Crédits</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="76"/>
+        <source>RTP-Driver</source>
+        <translation>Pilote RTP</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="77"/>
+        <source>Ralf Beck created an RTP-Driver which I use often to connect to the MIO10 when doing boring programming routines in front of an exciting film.
+As well he provided a patch to make MioConfig work with QT-Version 4.8.</source>
+        <translation>Ralf Beck a crée le pilote-RTP-Driver que j'utilise souvent pour connecter au MIO10 lorsque je fais des routines ennyueuses de programmation en face d'un super film.
+Il a fournit également une rustine pour faire que MioConfig fonctionne avec QT-Version.</translation>
+    </message>
+    <message>
+        <source>Ralf Beck created an RTP-Driver which I use often to connect to the MIO10 when doing boring programming routines in front of an exciting film.
+As well he provided a patch to make MioConfig work with QT-Version 4.8</source>
+        <translation type="vanished">Ralf Beck a crée le pilote-RTP-Driver que j'utilise souvent pour connecter au MIO10 lorsque je fais des routines ennyueuses de programmation en face d'un super film.
+Il a fournit également une rustine pour faire que MioConfig fonctionne avec QT-Version.</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceDetection</name>
+    <message>
+        <source>Dialog</source>
+        <translation type="vanished">Dialogue</translation>
+    </message>
+    <message>
+        <location filename="../src/devicedetection.ui" line="26"/>
+        <source>Device Detection</source>
+        <translation>Détection de périphérique.</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceInfoWidget</name>
+    <message>
+        <source>Device Info</source>
+        <translation type="vanished">Informationen de périphérique</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/deviceinfowidget.cpp" line="24"/>
+        <source>Global</source>
+        <translation>Global</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/deviceinfowidget.cpp" line="31"/>
+        <source>Network</source>
+        <translation>Réseau</translation>
+    </message>
+    <message>
+        <source>Networking</source>
+        <translation type="vanished">Réseautage</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/deviceinfowidget.h" line="15"/>
+        <source>Device Information</source>
+        <translation>Information de périphérique</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceWidget</name>
+    <message>
+        <source>Information</source>
+        <translation type="vanished">Informations/translation>
+    </message>
+    <message>
+        <source>Hello</source>
+        <translation type="vanished">Bonjour</translation>
+    </message>
+</context>
+<context>
+    <name>EthernetInfoWidget</name>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="77"/>
+        <source>Static Address</source>
+        <translation>Adresse statique</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="78"/>
+        <source>DHCP Address</source>
+        <translation>Adresse DHCP</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="79"/>
+        <source>Info</source>
+        <translation>Infos</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="81"/>
+        <source>Address Type</source>
+        <translation>Type d&apos;adresse</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="82"/>
+        <source>Static IP</source>
+        <translation>IP statique</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="83"/>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="86"/>
+        <source>Subnet Mask</source>
+        <translation>Masque de subnet</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="84"/>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="87"/>
+        <source>Gateway</source>
+        <translation>Passerelle</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="85"/>
+        <source>DHCP IP</source>
+        <translation>IP DHCP</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="88"/>
+        <source>Bonjour Name</source>
+        <translation>Nom Bonjour</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="89"/>
+        <source>MAC Address</source>
+        <translation>Adresse MAC</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="159"/>
+        <source>Static</source>
+        <translation>Statique</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/ethernetinfowidget.cpp" line="161"/>
+        <source>Dynamic</source>
+        <translation>Dynamique</translation>
+    </message>
+</context>
+<context>
+    <name>InfoItem</name>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="37"/>
+        <source>Accessory Name</source>
+        <translation>Nom accessoire</translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="39"/>
+        <source>Manufacturer Name</source>
+        <translation>Nom du fabricant</translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="41"/>
+        <source>Model Number</source>
+        <translation>Numéro de modèle</translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="43"/>
+        <source>Serial Number</source>
+        <translation>Numéro de série</translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="45"/>
+        <source>Firmware Version</source>
+        <translation>Version du micrologiciel</translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="47"/>
+        <source>Hardware Version</source>
+        <translation>Version du matériel</translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="49"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../src/sysex/retsetinfo.cpp" line="51"/>
+        <source>unknown</source>
+        <translation>inconnu</translation>
+    </message>
+</context>
+<context>
+    <name>InfoTableWidget</name>
+    <message>
+        <location filename="../src/widgets/infotablewidget.cpp" line="52"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/infotablewidget.cpp" line="53"/>
+        <source>Value</source>
+        <translation>Valeur</translation>
+    </message>
+</context>
+<context>
+    <name>MioMain</name>
+    <message>
+        <source>MioMain</source>
+        <translation type="vanished">MioMain</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.ui" line="26"/>
+        <source>iConnConfig</source>
+        <translation>iConnConfig</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.ui" line="49"/>
+        <source>File</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.ui" line="55"/>
+        <source>Device</source>
+        <translation>Périphérique</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.ui" line="71"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.ui" line="103"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.ui" line="108"/>
+        <source>Redetect Devices</source>
+        <translation>Redétecter les périphériques</translation>
+    </message>
+    <message>
+        <source>toolBar</source>
+        <translation type="vanished">barre d&apos;outils</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.ui" line="82"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.ui" line="87"/>
+        <source>Open Device</source>
+        <translation>Ouvrir le périphérique</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.ui" line="98"/>
+        <source>Device1</source>
+        <translation>Périphérique 1</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.ui" line="62"/>
+        <source>Select</source>
+        <translation>Sélectionner</translation>
+    </message>
+    <message>
+        <source>open device</source>
+        <translation type="obsolete">ouvrir le périphérique</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.cpp" line="165"/>
+        <source>Save current settings to device</source>
+        <translation>Sauvegarder les paramètres actuels dans le périphérique</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.cpp" line="174"/>
+        <source>Restore settings from device</source>
+        <translation>Restaurer les paramètres depuis le périphérique</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.cpp" line="183"/>
+        <source>Reset settings to factory default</source>
+        <translation>Réinitialiser les paramètres au défaut d&apos;usine</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.cpp" line="197"/>
+        <source>Device Actions</source>
+        <translation>Actions du périphérique</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.cpp" line="235"/>
+        <source>Store current setings to device?</source>
+        <translation>Stocker les paramètres actuels dans le périphérique ?</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.cpp" line="245"/>
+        <source>Waiting 10 seconds for device to be responsive again</source>
+        <translation>Attendre 10 secondes pour que le périphérique soit de nouveau réactif</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.cpp" line="246"/>
+        <source>Exit application</source>
+        <translation>Quitter l&apos;application</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.cpp" line="271"/>
+        <source>Read all settings from device?</source>
+        <translation>Lire tous les parapètres depuis le périphérique ?</translation>
+    </message>
+    <message>
+        <location filename="../src/iconnconfigmain.cpp" line="283"/>
+        <source>Reset all settings to factory default?</source>
+        <translation>Réinitialiser tous les paramètres aux valeurs d&apos;usine ?</translation>
+    </message>
+</context>
+<context>
+    <name>MultiInfoWidget</name>
+    <message>
+        <location filename="../src/widgets/multiinfowidget.ui" line="20"/>
+        <source>DockWidget</source>
+        <translation>DockWidget</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/multiinfowidget.h" line="24"/>
+        <source>MultiInfoWidget</source>
+        <translation>MultiInfoWidget</translation>
+    </message>
+</context>
+<context>
+    <name>PortSettingWidget</name>
+    <message>
+        <source>PortSettings</source>
+        <translation type="vanished">PortSettings</translation>
+    </message>
+</context>
+<context>
+    <name>PortsWidget</name>
+    <message>
+        <location filename="../src/widgets/portswidget.h" line="15"/>
+        <source>Ports</source>
+        <translation>Ports</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/main.cpp" line="34"/>
+        <source>filename to read settings from.</source>
+        <translation>nom du fichier dans lequel lire les paramètres.</translation>
+    </message>
+</context>
+</TS>
