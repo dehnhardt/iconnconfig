aeb817dfe92842a6eeb969c89397c26919cf88ab
Hoger Dehnhardt <holger@dehnhardt.org>
2019-02-22 17:59:45 +0100

    made buttons smaller and changed the color of not routet buttons

--- src/widgets/controls/portbutton.cpp
+++ src/widgets/controls/portbutton.cpp
@@ -6,66 +6,67 @@
 #include <QStylePainter>
 
 PortButton::PortButton(const long value, const QString iconText,
-											 const QString &text, const MidiPortType midiPortType,
-											 QWidget *parent)
-		: QToolButton(parent), m_iValue(value), m_sIconText(iconText),
-			m_MidiPortType(midiPortType) {
+					   const QString &text, const MidiPortType midiPortType,
+					   QWidget *parent)
+	: QToolButton(parent), m_iValue(value), m_sIconText(iconText),
+	  m_MidiPortType(midiPortType) {
 	m_Icon = PortDisplayHelper::getPortIcon(midiPortType);
 	//	setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
 	setToolButtonStyle(Qt::ToolButtonIconOnly);
 	setIcon(m_Icon);
-  setText(text);
+	setText(text);
 	setToolTip(text);
-  setCheckable(true);
-  setMinimumSize(50, 50);
+	setCheckable(true);
+	setMinimumSize(35, 35);
 	setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-  setIconSize(QSize(35, 35));
+	setIconSize(QSize(25, 25));
 }
 
 void PortButton::paintEvent(QPaintEvent *event) {
-  QToolButton::paintEvent(event);
+	QToolButton::paintEvent(event);
 	if (m_sIconText == "")
 		return;
-  QPainter painter(this);
-  QRect rect = event->rect();
-  QRect rect1 = this->rect();
+	QPainter painter(this);
+	QRect rect = event->rect();
+	QRect rect1 = this->rect();
 
 	int top = 0;
 
 	switch (m_MidiPortType) {
 	case DIN:
-		top = rect1.top() + 10;
+		top = rect1.top() + 6;
 		break;
 	case USB_HOST:
-		top = rect1.top() + 10;
+		top = rect1.top() + 7;
 		break;
 	case ETHERNET:
-		top = rect1.top() + 5;
+		top = rect1.top() + 4;
 		break;
 	default:
-		top = rect1.top() + 5;
+		top = rect1.top() + 3;
 		break;
 	}
 
-	QRect iconRect = QRect(rect1.left(), top, rect1.width(), iconSize().height());
+	QRect iconRect =
+		QRect(rect1.left(), top, rect1.width(), iconSize().height());
 
-  painter.setRenderHints(QPainter::SmoothPixmapTransform |
-                         QPainter::Antialiasing);
-  QFont font = painter.font();
-  font.setBold(true);
-  font.setPointSize(12);
-  painter.setFont(font);
-  if (isChecked()) {
-    QPen penHText(QColor("#66ff66"));
-    painter.setPen(penHText);
-  } else {
-    QPen penHText(QColor("#cccccc"));
-    painter.setPen(penHText);
-  }
-  if (rect1.intersects(rect))
+	painter.setRenderHints(QPainter::SmoothPixmapTransform |
+						   QPainter::Antialiasing);
+	QFont font = painter.font();
+	font.setBold(true);
+	font.setPointSize(9);
+	painter.setFont(font);
+	if (isChecked()) {
+		QPen penHText(QColor("#66ff66"));
+		painter.setPen(penHText);
+	} else {
+		QPen penHText(QColor("#dd8888"));
+		painter.setPen(penHText);
+	}
+	if (rect1.intersects(rect))
 		painter.drawText(iconRect, Qt::AlignCenter, m_sIconText);
 }
 
 long PortButton::getValue() const { return m_iValue; }
 
-void PortButton::setValue(long value) { value = value; }
+void PortButton::setValue(long value) { m_iValue = value; }
